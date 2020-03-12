e16ad4746fe6c95609e50c92b5c9cbda991c6df5
DoaaWael <45119186+DoaaWael@users.noreply.github.com>
2020-03-12 19:35:36 +0200

    add UI

---
+++ AR/Assets/openPanel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class openPanel : MonoBehaviour
+{
+    public GameObject panel;
+
+    Vector3 panelLocation;
+
+    private void Start()
+    {
+
+        panelLocation = panel.transform.position;
+        panelLocation.y = panelLocation.y + Screen.height;
+        LeanTween.move(panel, panelLocation, 0f);
+
+    }
+
+
+    public void OpenPanel()
+    {
+
+
+        if (panel != null)
+        {
+
+            bool isOpen = panel.activeSelf;
+            Animator animator = panel.GetComponent<Animator>();
+            if (isOpen)
+            {
+
+                LeanTween.move(panel, panelLocation, 1f).setEase(LeanTweenType.easeOutBack);
+                panel.SetActive(!isOpen);
+
+            }
+            else
+            {
+                panel.SetActive(true);
+                if (animator != null)
+                {
+                    // isOpen = animator.GetBool("open");
+                    animator.SetBool("open", true);
+                }
+                LeanTween.move(panel, new Vector3(panelLocation.x, panelLocation.y - Screen.height, panelLocation.z), 1f).setEase(LeanTweenType.easeOutBack);
+            }
+
+        }
+    }
+}
